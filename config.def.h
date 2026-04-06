80cd5d1a2b4e0b94875c8c71b2e14ea86a5b96b6
kullbachxyz <ph@kullbach.net>
2026-04-06 20:10:18 +0200

    add default program bindings

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[] 	    = { "monospace:size=11", "Noto Color Emoji:size=11", };
+static const char *fonts[] 	    = { "monospace:size=11", "Noto Color Emoji:size=11;antialias=true;autohint=true", };
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -67,12 +67,19 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[] = { "librewolf", NULL };
+static const char *emailcmd[] = { "st", "-e", "aerc", NULL };
+static const char *newscmd[] = { "st", "-e", "newsraft", NULL };
+static const char *filemancmd[] = { "st", "-e", "lf", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,             		XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_w,      spawn,          SHCMD(BROWSER) },
+	{ MODKEY,                       XK_w,      spawn,          {.v = browsercmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = emailcmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = filemancmd } },
+	{ MODKEY,                       XK_n,      spawn,          {.v = newscmd } },
 	{ MODKEY|ShiftMask,             XK_BackSpace, spawn,       SHCMD("sysact") },
 	{ MODKEY|ShiftMask,             XK_s,      spawn,          SHCMD("maimpick") },
 	{ MODKEY,             		XK_o,      spawn,          SHCMD("passmenu") },
