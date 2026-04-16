04488cb4e02d53c1dd34b6427e87a5ba8e56cadc
kullbachxyz <ph@kullbach.net>
2026-04-16 21:37:20 +0200

    change mail from aerc to neomutt

--- config.def.h
+++ config.def.h
@@ -68,7 +68,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "librewolf", NULL };
-static const char *emailcmd[] = { "st", "-e", "aerc", NULL };
+static const char *emailcmd[] = { "st", "-e", "neomutt", NULL };
 static const char *newscmd[] = { "st", "-e", "newsraft", NULL };
 static const char *filemancmd[] = { "st", "-e", "lf", NULL };
 
