3151c9e6ecac0178ea97cfe2e4c9887e087a2071
kullbachxyz <ph@kullbach.net>
2026-04-20 19:36:15 +0200

    fix font definition

--- config.def.h
+++ config.def.h
@@ -12,7 +12,10 @@ static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[] 	    = { "monospace:size=11", "Noto Color Emoji:size=11;antialias=true;autohint=true", };
+static const char *fonts[] = {
+	"monospace:size=11",
+	"Noto Color Emoji:size=11",
+};
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
