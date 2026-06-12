57a8737b3bda44e3af66d80c3170615035779b44
kullbachxyz <ph@kullbach.net>
2026-06-12 22:39:57 +0200

    actualfullscreen patch

--- config.def.h
+++ config.def.h
@@ -11,6 +11,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[] = {
@@ -28,7 +29,7 @@ static const char col_mauve[]       = "#cba6f7";
 static const char col_lavender[]    = "#b4befe";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_text,     col_crust,  col_crust   },
+	[SchemeNorm] = { col_text,     col_crust,  col_surface0 },
 	[SchemeSel]  = { col_text,     col_crust,  col_lavender  },
 	[SchemeTag]  = { col_overlay0, col_crust,  col_crust   },
 };
@@ -41,15 +42,17 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",     NULL,       NULL,           0,         1,          0,          0,         -1 },
+	{ "LibreWolf", NULL,      NULL,           1 << 8,    0,          0,          0,         -1 },
+	{ "st-256color", NULL,    NULL,           0,         0,          1,          0,         -1 },
+	{ NULL,       NULL,       "Event Tester", 0,         0,          0,          1,         -1 },
 };
 
 /* layout(s) */
 static const float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 static const int refreshrate = 120;  /* refresh rate (per second) for client move/resize */
 
@@ -70,8 +73,6 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-
-#define STATUSBAR "dwmblocks"
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
@@ -113,6 +114,7 @@ static const Key keys[] = {
 	/*{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },*/
 	/*{ MODKEY,                       XK_space,  setlayout,      {0} },*/
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
+	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_Left,   focusmon,       {.i = -1 } },
