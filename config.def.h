6e810ad84a27e21189ef9ab865a0dae499816fbf
kullbachxyz <ph@kullbach.net>
2026-06-20 10:21:29 +0200

    vanitygaps patch

--- config.def.h
+++ config.def.h
@@ -10,6 +10,11 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappih    = 8;        /* horiz inner gap between windows */
+static const unsigned int gappiv    = 8;        /* vert inner gap between windows */
+static const unsigned int gappoh    = 8;        /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 8;        /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only 1 window */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -113,6 +118,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_z,	     zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_a,      togglegaps,     {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,	                      XK_q,      killclient,     {0} },
 	/*{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },*/
