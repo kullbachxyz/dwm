13319765dffb4186c034c9c6d1a1466ff66d191b
kullbachxyz <ph@kullbach.net>
2026-07-02 22:14:23 +0200

    add default play/pause bindings

--- config.def.h
+++ config.def.h
@@ -161,6 +161,8 @@ static const Key keys[] = {
 	{ 0, XF86XK_AudioLowerVolume,  	spawn,     SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_AudioRaiseVolume,  	spawn,     SHCMD("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+; kill -44 $(pidof dwmblocks)") },
 	{ 0, XF86XK_AudioMute,         	spawn,     SHCMD("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle; kill -44 $(pidof dwmblocks)") },
+	{ 0, XF86XK_AudioPlay,         	spawn,     SHCMD("player-toggle") },
+	{ 0, XF86XK_AudioPause,        	spawn,     SHCMD("player-toggle") },
 	{ 0, XF86XK_MonBrightnessDown, 	spawn,     SHCMD("brightnessctl set 10-") },
 	{ 0, XF86XK_MonBrightnessUp,   	spawn,     SHCMD("brightnessctl set 10+") },
 };
